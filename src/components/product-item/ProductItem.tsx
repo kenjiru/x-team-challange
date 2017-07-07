4de90cd89cf32659b301d5b82e3cfd9d716ed7f0
kenjiru <kenjiru.ro@gmail.com>
2017-07-08 00:42:50 +0200

    Request the initial batch of items.

--- src/components/product-item/ProductItem.tsx
+++ src/components/product-item/ProductItem.tsx
@@ -7,18 +7,27 @@ import "./ProductItem.less";
 
 class ProductItem extends React.Component<IProductItemProps, IProductItemState> {
     public render(): React.ReactElement<any> {
-        let style: object = {
-            fontSize: this.props.product.size
-        };
-
         return (
-            <div className="product-item">
-                <div className="face" style={style}>{this.props.product.face}</div>
+            <div className="product-item" style={this.getItemStyle()}>
+                <div className="face" style={this.getFaceStyle()}>{this.props.product.face}</div>
                 <div className="id">id: {this.props.product.id}</div>
                 <div className="price">price: {this.props.product.price}</div>
                 <div className="date">added: {this.getDate()} ago</div>
             </div>
         );
+    }
+
+    private getItemStyle(): Object {
+        return {
+            height: this.props.height,
+            width: this.props.width
+        };
+    }
+
+    private getFaceStyle(): Object {
+        return {
+            fontSize: this.props.product.size
+        };
     }
 
     private getDate(): string {
@@ -31,6 +40,8 @@ interface IProductItemState {
 
 export interface IProductItemProps {
     product: IProduct;
+    width: number;
+    height: number;
 }
 
 export default ProductItem;
--- src/components/product-list/ProductList.tsx
+++ src/components/product-list/ProductList.tsx
@@ -11,6 +11,21 @@ import ScrollContainer from "../scroll-container/ScrollContainer";
 import "./ProductList.less";
 
 export class ProductList extends React.Component<IProductListProps, IProductListState> {
+    private static PRODUCT_WIDTH: number = 160;
+    private static PRODUCT_HEIGHT: number = 160;
+
+    private containerWidth: number = 0;
+    private containerHeight: number = 0;
+
+    public componentDidMount(): void {
+        console.log(this.state);
+
+        let initialNumElements: number = this.determineInitialNumElements();
+
+        this.props.shopStore.requestItems(initialNumElements);
+        this.props.shopStore.addRequestedItems();
+    }
+
     public render(): React.ReactElement<any> {
         const shopStore: ShopStore = this.props.shopStore;
 
@@ -20,11 +35,14 @@ export class ProductList extends React.Component<IProductListProps, IProductList
                     Number of items:
                     <span className="num-items">{shopStore.items.length}</span>
                 </div>
-                <ScrollContainer className="list" onLoading={this.handleLoading}
-                                 canLoad={this.canLoad()}
-                                 onDisplay={this.handleDisplay}>
-                    {this.renderAllItems()}
-                </ScrollContainer>
+                <div ref={this.handleListRef}>
+                    <ScrollContainer className="list"
+                                     onLoading={this.handleLoading}
+                                     canLoad={this.canLoad()}
+                                     onDisplay={this.handleDisplay}>
+                        {this.renderAllItems()}
+                    </ScrollContainer>
+                </div>
                 <LoadingIndicator/>
                 <button onClick={this.getProducts}>Get items</button>
             </div>
@@ -36,16 +54,22 @@ export class ProductList extends React.Component<IProductListProps, IProductList
     }
 
     private renderItem(product: IProduct): ReactElement<any> {
-        return <ProductItem key={product.id} product={product}/>;
+        return <ProductItem key={product.id} product={product} width={ProductList.PRODUCT_WIDTH}
+                            height={ProductList.PRODUCT_HEIGHT}/>;
+    }
+
+    private handleListRef = (elem: any): any => {
+        if (_.isNil(elem) === false) {
+            this.containerWidth = elem.clientWidth;
+            this.containerHeight = elem.clientHeight;
+        }
     }
 
     private handleLoading = (): void => {
-        console.log("handleLoading");
         this.props.shopStore.requestItems();
     }
 
     private handleDisplay = (): void => {
-        console.log("handleDisplay");
         this.props.shopStore.addRequestedItems();
     }
 
@@ -58,6 +82,13 @@ export class ProductList extends React.Component<IProductListProps, IProductList
     private getProducts = (): void => {
         this.props.shopStore.requestItems();
         this.props.shopStore.addRequestedItems();
+    }
+
+    private determineInitialNumElements(): number {
+        const numItemsOnRow: number = Math.floor(this.containerWidth / ProductList.PRODUCT_WIDTH);
+        const numRows: number = Math.ceil(this.containerHeight / ProductList.PRODUCT_HEIGHT);
+
+        return numItemsOnRow * numRows;
     }
 }
 
