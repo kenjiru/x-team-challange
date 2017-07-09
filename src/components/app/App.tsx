9ac5d4d6b2cea79a1ac3911e8598a0f422219635
kenjiru <kenjiru.ro@gmail.com>
2017-07-09 23:33:50 +0200

    Use the provided template.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -12,8 +12,20 @@ class App extends React.Component<any, any> {
         return (
             <Provider shopStore={shopStore}>
                 <div className="app">
-                    <h1>Discount Ascii Warehouse</h1>
-                    <ProductList/>
+                    <header>
+                        <h1>Discount Ascii Warehouse</h1>
+
+                        <p>Here you're sure to find a bargain on some of the finest ascii available to purchase.
+                            Be sure to peruse our selection of ascii faces in an exciting range of sizes and prices.</p>
+
+                        <p>But first, a word from our sponsors:</p>
+
+                        <img className="ad" src={"/ad/?r=" + Math.floor(Math.random() * 1000)}/>
+                    </header>
+
+                    <section className="products">
+                        <ProductList/>
+                    </section>
                 </div>
             </Provider>
         );
