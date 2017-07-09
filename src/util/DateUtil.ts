340c1ceda8e245c941baabb8331f509d4e96c1fe
kenjiru <kenjiru.ro@gmail.com>
2017-07-09 14:33:52 +0200

    Format the input date to avoid moment warnings.

--- src/util/DateUtil.ts
+++ src/util/DateUtil.ts
@@ -1,21 +1,24 @@
-import * as Moment from "moment";
+import * as moment from "moment";
+import {Moment} from "moment";
 import {duration, Duration} from "moment";
 import {DateRange, extendMoment, MomentRangeExtends} from "moment-range";
 
-const moment: MomentRangeExtends & Moment.Moment = extendMoment(Moment);
+const momentRange: MomentRangeExtends & moment.Moment = extendMoment(moment);
 
 class DateUtil {
     public static DATE_FORMAT: string = "YYYY-MM-DD";
+    private static DATE_INPUT_FORMAT: string = "ddd MMM D YYYY HH:mm:ss ZZ";
 
     public static formatDate(date: string): string {
-        let dealRange: DateRange = moment.range(moment(), moment(date));
+        let momentDate: Moment = momentRange(date, DateUtil.DATE_INPUT_FORMAT);
+        let dealRange: DateRange = momentRange.range(momentRange(), momentDate);
         let timeDuration: Duration = duration(dealRange.valueOf());
 
         if (timeDuration.asDays() > -7) {
             return timeDuration.humanize() + " ago";
         }
 
-        return moment(date).format(DateUtil.DATE_FORMAT);
+        return momentRange(date).format(DateUtil.DATE_FORMAT);
     }
 }
 
